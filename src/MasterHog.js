f8c083500f3d23bc0ae2f1777e36483441bfc9db
github username <github email address>
2019-10-09 10:38:40 -0400

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -14,8 +14,14 @@ export default class MasterHog extends Component {
 
 
   changeEyeColor = (e) => {
+    console.log(e.target)
     this.setState({
       eyeColor: e.target.value
+    })
+  }
+  showOffspring = () => {
+    return offspring.map(baby => {
+      return <BabyHog key={baby.id} name={baby.name} hobby={baby.hobby} eyeColor={this.state.eyeColor}/>
     })
   }
 
@@ -30,19 +36,17 @@ export default class MasterHog extends Component {
         Sun<br></br>
         <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          {this.showOffspring()}
         </ul>
 
       </div>
